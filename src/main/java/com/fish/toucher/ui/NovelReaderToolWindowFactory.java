da82f9a99ebed08a837d18c1f51dd6144999b796
fengshi <www@qkw.io>
2026-03-27 03:15:04 +0100

    修复bug

--- src/main/java/com/fish/toucher/ui/NovelReaderToolWindowFactory.java
+++ src/main/java/com/fish/toucher/ui/NovelReaderToolWindowFactory.java
@@ -4,11 +4,14 @@ import com.intellij.openapi.Disposable;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.content.ContentManager;
 import com.fish.toucher.settings.NovelReaderSettings;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,19 +22,38 @@ public class NovelReaderToolWindowFactory implements ToolWindowFactory, DumbAwar
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         LOG.info("createToolWindowContent: creating tool window for project " + project.getName());
+        buildContent(project, toolWindow);
+        LOG.info("createToolWindowContent: tool window created successfully");
+    }
+
+    private static void buildContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         NovelReaderSettings settings = NovelReaderSettings.getInstance();
+        ContentManager cm = toolWindow.getContentManager();
+        cm.removeAllContents(true);
 
         if (settings.isHotSearchMode()) {
             HotSearchPanel hotSearchPanel = new HotSearchPanel(project);
             Content content = ContentFactory.getInstance().createContent(hotSearchPanel, "", false);
-            toolWindow.getContentManager().addContent(content);
+            cm.addContent(content);
             Disposer.register(toolWindow.getDisposable(), (Disposable) hotSearchPanel::dispose);
         } else {
             NovelReaderPanel panel = new NovelReaderPanel(project);
             Content content = ContentFactory.getInstance().createContent(panel, "", false);
-            toolWindow.getContentManager().addContent(content);
+            cm.addContent(content);
         }
+    }
 
-        LOG.info("createToolWindowContent: tool window created successfully");
+    /**
+     * Rebuild tool window content for all open projects (called after mode switch).
+     */
+    public static void rebuildAllToolWindows() {
+        for (Project project : ProjectManager.getInstance().getOpenProjects()) {
+            if (project.isDisposed()) continue;
+            ToolWindow tw = ToolWindowManager.getInstance(project).getToolWindow("Fish Toucher");
+            if (tw != null) {
+                buildContent(project, tw);
+                LOG.info("rebuildAllToolWindows: rebuilt for project " + project.getName());
+            }
+        }
     }
 }
